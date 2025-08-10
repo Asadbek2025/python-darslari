ec36734748a03437fb2dc673bede58978250844d
Asadbek2025 <joraqulovasadbek1999@gmail.com>
2025-08-10 06:21:50 +0500

    yangi fayllar qoshildi
    temp.pyda ozgarishlar boldi

---
+++ temp.py
@@ -0,0 +1,38 @@
+# def multiply(*sonlar):
+#     kopaytma=1
+#     for son in sonlar:
+#         kopaytma*=son
+#     return kopaytma
+# print(multiply(7,8,10))
+# def info(ism,familya,**qoshimcha):
+#     qoshimcha['ismi']=ism
+#     qoshimcha['familyasi']=familya
+#     return qoshimcha
+# print(info('asadbek','joraqulov',tyil=1999,tsana=4,mutaxasisligi='it'))
+def bahola(ismlar):
+    baholar=[]
+    for ism in ismlar:
+        baho=input(f'talaba {ism.title()}ning bahosi')
+        baholar[ism]=baho     
+    return baholar
+talabalar=['ali','vali','hasan','husan']
+baholar=bahola(talabalar)
+print(baholar)    
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
